384270b9fbb828888283aacf0625755ff79a76cb
gyursel <87885104+gyursel@users.noreply.github.com>
2026-09-05 16:32:29 +0300

    Enable admin logo manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/hero-fix.css';
+import LogoManager from '../lib/LogoManager';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <LogoManager />
+    </>
+  );
 }
