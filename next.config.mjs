2a1404acdcbb8532b9127be7a1c9f51762bf6d2b
gyursel <87885104+gyursel@users.noreply.github.com>
2026-09-08 05:50:42 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
